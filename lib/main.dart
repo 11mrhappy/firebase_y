65f9309222b13a8bed23f0725e961bec2bda49da
11mrhappy <nekomamiremy@gmail.com>
2021-03-01 23:03:51 +0900

    画面遷移作成

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import './screen/register_page.dart';
+import './screen/signin_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter chatapp textbook',
+      initialRoute: RegisterPage.id,
+      routes: {
+        RegisterPage.id: (context) => RegisterPage(),
+        SignInPage.id: (context) => SignInPage(),
+      }
+    );
+  }
+}
